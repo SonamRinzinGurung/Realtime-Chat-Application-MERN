6f5837cfd27f8dd975a9b3184f165e7bffbeabbf
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-09-15 18:34:02 +0545

    Register User UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register, Login, Chat } from "./pages";
 function App() {
-  return <h1>app</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Chat from "./Chat";
+
+import Login from "./Login";
+import Register from "./Register";
+
+export { Chat, Login, Register };
