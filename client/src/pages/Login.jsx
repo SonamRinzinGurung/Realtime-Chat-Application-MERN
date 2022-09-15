d12432066f42f3bc26663b5eda4530d476361875
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-09-15 22:06:51 +0545

    Login UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, password } = values;
 
-export default Login
+    if (handleValidation()) {
+      const { data } = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        {
+          username,
+
+          password,
+        }
+      );
+      if (data.status === false) {
+        toast.error(data.message, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/");
+      }
+    }
+  };
+
+  const handleValidation = () => {
+    const { username, password } = values;
+
+    if (username.length === 0) {
+      toast.error("Username is required", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Password is required", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  return (
+    <>
+      <FormContainer>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="brand">
+            <img src={Logo} alt="" />
+            <h1>Chat App</h1>
+          </div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => handleChange(e)}
+            min="6"
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => handleChange(e)}
+          />
+
+          <button type="submit">Login</button>
+          <span>
+            don't have an account ? <Link to={"/register"}>Register</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
+    </>
+  );
+};
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+  }
+  img {
+    height: 5rem;
+  }
+  h1 {
+    color: #fff;
+    text-transform: uppercase;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #997af0;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    transition: 0.5s ease-in-out;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: #fff;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
+
+export default Login;
